94afe7aa579acda3f22befc8223b6ddb21c27e1d
damast100 <dmast5013@gmail.com>
2022-04-12 21:15:51 -0400

    adding updated files

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,29 +4,47 @@ import csv
 path = "C:\\Users\\Derek\\Desktop\\"
 budget_csv = os.path.join(path, 'python-challenge', 'PyBank', 'Resources', 'budget_data.csv')
 
+# create lists to retain the information
 total_months = []
 total_revenue = []
 
-#Count the total months and revenue
+# open and read the CSV file
 with open(budget_csv, 'r') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
+
+	# skip the header
 	header = next(csvreader)
+
+	# iterate through each row in the CSV file
 	for row in csvreader:
+
+	# add the number of lines to the lists to get the sum at a later point
 		total_months.append(row[0])
 		total_revenue.append(int(row[1]))
 
+# print statements
 print("Financial Analysis")
 print("------------------------")
 print(f'Total Months: {len(total_months)}')
 print(f'Total: ${sum(total_revenue)}')
 
+
+# create lists to retain the information
 net_change_list = []
 date_change_list = []
+
+# open and read the CSV file a second time
 with open(budget_csv, 'r') as csvfile:
 	csvreader = csv.reader(csvfile)
+
+	# skip the header
 	header = next(csvreader)
+
+	#skip the first row
 	first_row = next(csvreader)
 	prev_net = int(first_row[1])
+
+	# iterate through each row in the CSV file to find the change in values over time
 	for row in csvreader:
 		net_change = int(row[1]) - prev_net
 		prev_net = int(row[1])
@@ -34,6 +52,7 @@ with open(budget_csv, 'r') as csvfile:
 		date_change_list.append(row[0])
 	net_average = round((sum(net_change_list) / len(net_change_list)), 2)
 
+# creating variables with calculations to make the print statements easier
 increase = max(net_change_list)
 decrease = min(net_change_list)
 increase_index = net_change_list.index(max(net_change_list))
@@ -41,10 +60,12 @@ decrease_index = net_change_list.index(min(net_change_list))
 month_increase = date_change_list[increase_index]
 month_decrease = date_change_list[decrease_index]
 
+# print statements
 print(f'Average Change: ${net_average}')
 print(f'Greatest Increase in Profits: {month_increase} (${increase})')
 print(f'Greatest Decrease in Profits: {month_decrease} (${decrease})')
 
+# writing the information to the new file
 output_file = os.path.join(path, 'python-challenge', 'PyBank', 'Analysis', 'financial_analysis.txt')
 with open(output_file, "w") as file:
 	file.write("Financial Analysis")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,15 +4,26 @@ import csv
 path = "C:\\Users\\Derek\\Desktop\\"
 election_csv = os.path.join(path, 'python-challenge', 'PyPoll', 'Resources', 'election_data.csv')
 
+# create lists to retain the information
 votes = []
 name_charles = []
 name_diana = []
 name_raymon = []
+
+# open and read the CSV file
 with open(election_csv, 'r') as csvfile:
 	csvreader = csv.reader(csvfile)
+	
+	# skip the header
 	header = next(csvreader)
+
+	# iterate through each row in the CSV file
 	for row in csvreader:
+
+		# add the number of lines to the list to get the sum at a later point
 		votes.append(int(row[0]))
+
+		#looking to see if these names appear and adding them to the lists to get the sum at a later point
 		if row[2] == "Charles Casper Stockham":
 			name_charles.append(row[2])
 		elif row[2] == "Diana DeGette":
@@ -20,6 +31,7 @@ with open(election_csv, 'r') as csvfile:
 		elif row[2] == "Raymon Anthony Doane":
 			name_raymon.append(row[2])
 
+# creating variables with calculations to make the print statements easier
 total_votes = len(votes)
 charles_votes = len(name_charles)
 diana_votes = len(name_diana)
@@ -28,7 +40,34 @@ charles_percentage = round(((charles_votes / total_votes) * 100), 3)
 diana_percentage = round(((diana_votes / total_votes) * 100), 3)
 raymon_percentage = round(((raymon_votes / total_votes) * 100), 3)
 
-print(total_votes)
-print(f'Percentage: {charles_percentage}%')
-print(diana_percentage)
-print(raymon_percentage)
+# print statements
+print("Election Results")
+print("----------------------")
+print(f'Total Votes: {total_votes}')
+print(f'Charles Casper Stockham: {charles_percentage}% ({charles_votes})')
+print(f'Diana DeGette: {diana_percentage}% ({diana_votes})')
+print(f'Raymon Anthony Doane: {raymon_percentage}% ({raymon_votes})')
+print("---------------------")
+print(f'Winner : Diana DeGette')
+print("----------------------")
+
+# writing the information to the new file
+output_file = os.path.join(path, 'python-challenge', 'PyPoll', 'Analysis', 'financial_analysis_election.txt')
+with open (output_file, "w") as file:
+	file.write("Election Results")
+	file.write("\n")
+	file.write("----------------------")
+	file.write("\n")
+	file.write(f'Total Votes: {total_votes}')
+	file.write("\n")
+	file.write(f'Charles Casper Stockham: {charles_percentage}% ({charles_votes})')
+	file.write("\n")
+	file.write(f'Diana DeGette: {diana_percentage}% ({diana_votes})')
+	file.write("\n")
+	file.write(f'Raymon Anthony Doane: {raymon_percentage}% ({raymon_votes})')
+	file.write("\n")
+	file.write("---------------------")
+	file.write("\n")
+	file.write(f'Winner : Diana DeGette')
+	file.write("\n")
+	file.write("----------------------")
